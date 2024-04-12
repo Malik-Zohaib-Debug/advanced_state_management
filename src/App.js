0076b9187ce28027c40d4a2eed8b115b93b380e8
Malik Zohaib Zahid <64071105+Malik-Zohaib-Debug@users.noreply.github.com>
2024-04-12 22:00:38 +0500

    initial commit with react redux

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { useDispatch, useSelector } from "react-redux";
 
 function App() {
+  const counter = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+
+  const increment = () => {
+    dispatch({type: "INCREMENT"})
+  }
+
+  const decrement = () => {
+    dispatch({type: "DECREMENT"})
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{textAlign: "center", justifyContent: "center"}}>
+      Advanced State Management With React Redux
+      <div>
+        Counter : {counter}
+        <div style={{justifyContent: "space-between"}}>
+          <button onClick={increment}>Increment +</button>
+          <button onClick={decrement}>Decrement -</button>
+        </div>
+      </div>
     </div>
   );
 }
